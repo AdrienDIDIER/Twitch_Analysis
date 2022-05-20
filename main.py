6abb66b678df72ac02abc4b168a4c5f944b9c400
adrienDidier <adrien.didier@outlook.fr>
2022-05-20 11:05:09 +0200

    VOD

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import twitch
 import pytz
 import twitch_api
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from dotenv import load_dotenv
-
+from pytimeparse.timeparse import timeparse
 
 load_dotenv()
 
@@ -59,8 +59,10 @@ async def get_vod_streamer(request: Request, name: str = Form()):
   return templates.TemplateResponse("list_vods.html", {"request": request, "list_vods": list_vods})
 
 @app.post("/vod", response_class=HTMLResponse)
-async def home(request: Request, vod_id: int = Form()):
+async def home(request: Request, vod_id: int = Form(), vod_old_id: int = Form()):
   infos_vod = twitch_api.read_info_vod(vod_id)
+  old_infos_vod = twitch_api.read_info_vod(vod_old_id)
+
   twitch_api.dl_vod(vod_id)
   df = twitch_api.read_chat_vod(vod_id)
   df_t = twitch_api.treatment_df(df)
@@ -70,10 +72,19 @@ async def home(request: Request, vod_id: int = Form()):
     'labels' : df_v['date'].tolist(),
     'data' : df_v['nb_messages'].tolist()
   }
+  
+  s1 = timeparse(infos_vod['duration'])
+  s2 = timeparse(old_infos_vod['duration'])
+ 
+  time_1 = timedelta(seconds=s1)
+  time_2 = timedelta(seconds=s2)
 
   data = {}
   data['infos_vod'] = infos_vod
+  data['evol'] = {
+    'evol_view_count' : round(((float(infos_vod['view_count']) - float(old_infos_vod['view_count']))/float(old_infos_vod['view_count']))*100,2),
+    'evol_duration' : round(((time_1 - time_2)/time_2)*100,2),
+  }
   data['infos_chart'] = infos_chart
-
-  print(data)
+  
   return templates.TemplateResponse("vod.html", {"request": request, "vod_data": data})
